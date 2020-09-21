3525d87902c91f4be5cf8614fe6502021c2aca95
Yoshihiro Ito <yoship1639@gmail.com>
2020-09-21 20:36:51 +0900

    fix

--- Player2VRM/Player2VRM.cs
+++ Player2VRM/Player2VRM.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using DG.Tweening;
+using HarmonyLib;
 using Oc;
 using System;
 using System.Collections;
@@ -184,9 +185,29 @@ namespace Player2VRM
             {
                 normalizedTimeOffset = (pl.getAct<OcPl.As>(OcPl.As.MovementStand) as AsPl_MovementStand).RunEndMotRate;
             }
+            //__instance.animPlaySec(OcAnimHash.Sprint, normalizedTimeOffset, 0, 0.1f);
             pl.Animator.CrossFade(OcAnimHash.Sprint, 0.1f, 0, normalizedTimeOffset);
             OcPlVRM.modelMaster.GetComponentInChildren<Animator>().CrossFade(OcAnimHash.Sprint, 0.1f, 0, normalizedTimeOffset);
             return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(AsPl_MovementStand))]
+    [HarmonyPatch("animPlayMovemnt")]
+    static class AsPl_MovementStandVRM
+    {
+        static bool Prefix(AsPl_MovementStand __instance)
+        {
+            if (OcPlVRM.modelMaster == null) return true;
+
+            var pl = __instance.GetRefField<OcActState_Pl, OcPl>("_Pl");
+            if (pl.isActPrev<OcPl.As>(OcPl.As.Sprint))
+            {
+                AsPl_Sprint asPl_Sprint = pl.getAct<OcPl.As>(OcPl.As.Sprint) as AsPl_Sprint;
+                OcPlVRM.modelMaster.GetComponentInChildren<Animator>().CrossFade(OcAnimHash.MovementStand, 0.6f, 0, asPl_Sprint.SprintEndMotRate);
+            }
+
+            return true;
         }
     }
 
@@ -211,8 +232,149 @@ namespace Player2VRM
     {
         static bool Prefix(ShaderStore __instance, ref Shader __result, glTFMaterial material)
         {
-            __result = Shader.Find("RealToon/Version 5/Default/Default");
+            if (material == null)
+            {
+                __result = Shader.Find("Standard");
+                return false;
+            }
+            if (material.extensions != null && material.extensions.KHR_materials_unlit != null)
+            {
+                __result = Shader.Find("RealToon/Version 5/Default/Default");
+                return false;
+            }
+            __result = Shader.Find("Standard");
             return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(MaterialImporter))]
+    [HarmonyPatch("CreateMaterial")]
+    static class MaterialImporterVRM
+    {
+        static void Postfix(MaterialImporter __instance, ref Material __result, int i, glTFMaterial x, bool hasVertexColor)
+        {
+            __result.SetFloat("_DoubleSided", x.doubleSided ? 0 : 2);
+            __result.SetFloat("_Cutout", x.alphaCutoff);
+
+            if (x.pbrMetallicRoughness != null)
+            {
+                if (x.pbrMetallicRoughness.baseColorFactor != null && x.pbrMetallicRoughness.baseColorFactor.Length == 3)
+                {
+                    float[] baseColorFactor2 = x.pbrMetallicRoughness.baseColorFactor;
+                    __result.SetColor("_MainColor", new Color(baseColorFactor2[0], baseColorFactor2[1], baseColorFactor2[2]).gamma);
+                }
+                else if (x.pbrMetallicRoughness.baseColorFactor != null && x.pbrMetallicRoughness.baseColorFactor.Length == 4)
+                {
+                    float[] baseColorFactor2 = x.pbrMetallicRoughness.baseColorFactor;
+                    __result.SetColor("_MainColor", new Color(baseColorFactor2[0], baseColorFactor2[1], baseColorFactor2[2], baseColorFactor2[3]).gamma);
+                }
+                __result.SetFloat("_RefMetallic", x.pbrMetallicRoughness.metallicFactor);
+            }
+            
+
+            if (x.normalTexture != null && x.normalTexture.index != -1)
+            {
+                __result.SetFloat("_N_F_NM", 1.0f);
+                var func = __instance.GetRefField<MaterialImporter, Func<int, TextureItem>>("GetTextureFunc");
+                TextureItem textureItem4 = func(x.normalTexture.index);
+                if (textureItem4 != null)
+                {
+                    string text2 = "_NormalMap";
+                    __result.SetTexture(text2, textureItem4.ConvertTexture(text2, 1f));
+                    __result.SetFloat("_NormalMapIntensity", x.normalTexture.scale);
+                }
+                SetTextureOffsetAndScale(__result, x.normalTexture, "_NormalMap");
+            }
+        }
+
+        private static void SetTextureOffsetAndScale(Material material, glTFTextureInfo textureInfo, string propertyName)
+        {
+            if (textureInfo.extensions != null && textureInfo.extensions.KHR_texture_transform != null)
+            {
+                glTF_KHR_texture_transform khr_texture_transform = textureInfo.extensions.KHR_texture_transform;
+                Vector2 vector = new Vector2(0f, 0f);
+                Vector2 vector2 = new Vector2(1f, 1f);
+                if (khr_texture_transform.offset != null && khr_texture_transform.offset.Length == 2)
+                {
+                    vector = new Vector2(khr_texture_transform.offset[0], khr_texture_transform.offset[1]);
+                }
+                if (khr_texture_transform.scale != null && khr_texture_transform.scale.Length == 2)
+                {
+                    vector2 = new Vector2(khr_texture_transform.scale[0], khr_texture_transform.scale[1]);
+                }
+                vector.y = (vector.y + vector2.y - 1f) * -1f;
+                material.SetTextureOffset(propertyName, vector);
+                material.SetTextureScale(propertyName, vector2);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(OcPlEquip))]
+    [HarmonyPatch("setDraw")]
+    static class OcPlEquipVRM
+    {
+        static bool Prefix(OcPlEquip __instance, ref bool isDraw)
+        {
+            var str = Settings.ReadSettings("DrawEquipHead");
+            var flag = true;
+            if (__instance.EquipSlot == OcEquipSlot.EqHead && bool.TryParse(str, out flag))
+            {
+                if (!flag)
+                {
+                    isDraw = false;
+                    return true;
+                }
+            }
+
+            str = Settings.ReadSettings("DrawEquipAccessory");
+            flag = true;
+            if (__instance.EquipSlot == OcEquipSlot.Accessory && bool.TryParse(str, out flag))
+            {
+                if (!flag)
+                {
+                    isDraw = false;
+                    return true;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class AnimFitter : MonoBehaviour
+    {
+        private OcPl pl;
+        public Animator masterAnim;
+
+        void Start()
+        {
+            pl = GetComponent<OcPl>();
+        }
+
+        private AnimatorStateInfo prevInfo;
+        void Update()
+        {
+            var info = pl.Animator.GetCurrentAnimatorStateInfo(0);
+            var time = info.normalizedTime;
+
+            masterAnim.speed = pl.Animator.speed;
+            if (prevInfo.shortNameHash != info.shortNameHash && info.shortNameHash == OcAnimHash.MovementStand) masterAnim.ForceStateNormalizedTime(time);
+            if (prevInfo.shortNameHash != info.shortNameHash && info.shortNameHash == OcAnimHash.Sprint) masterAnim.ForceStateNormalizedTime(time);
+
+            prevInfo = info;
+        }
+
+        public static void ShowHierarchy(Transform root, int num = 0)
+        {
+            if (num == 0) UnityEngine.Debug.LogError("ShowHierarchy ------------------------------- Start");
+            string str = "";
+            for (var i = 0; i < num; i++) str += "  ";
+            UnityEngine.Debug.LogWarning(str + root.name + $"【{root.gameObject.layer}】" + " (" + root.GetComponents<Component>().Join(c => c.GetType().Name) + ")" + root.gameObject.activeInHierarchy);
+            for (var i = 0; i < root.childCount; i++)
+            {
+                ShowHierarchy(root.GetChild(i), num + 1);
+            }
+            if (num == 0) UnityEngine.Debug.LogError("ShowHierarchy ------------------------------- End");
         }
     }
 
@@ -244,6 +406,17 @@ namespace Player2VRM
                 {
                     smr.receiveShadows = false;
                 }
+
+                // プレイヤースケール調整
+                {
+                    var scaleStr = Settings.ReadSettings("PlayerScale");
+                    var scale = 1.0f;
+                    if (scaleStr != null && float.TryParse(scaleStr, out scale))
+                    {
+                        __instance.transform.localScale *= scale;
+                        vrmModel.transform.localScale /= scale;
+                    }
+                }
             }
 
             foreach (var smr in __instance.GetComponentsInChildren<SkinnedMeshRenderer>())
@@ -268,15 +441,33 @@ namespace Player2VRM
             if (__instance is OcPlMaster)
             {
                 if (modelMaster == null) modelMaster = GameObject.Instantiate<GameObject>(vrmModel);
+                var instAnim = __instance.GetComponentInChildren<Animator>();
+                var masterAnim = modelMaster.GetComponentInChildren<Animator>();
                 modelMaster.transform.SetParent(__instance.transform, false);
-                modelMaster.GetComponentInChildren<Animator>().runtimeAnimatorController = __instance.GetComponentInChildren<Animator>().runtimeAnimatorController;
+                masterAnim.runtimeAnimatorController = instAnim.runtimeAnimatorController;
+
+                var fitter = __instance.GetComponent<AnimFitter>();
+                if (fitter == null)
+                {
+                    fitter = __instance.gameObject.AddComponent<AnimFitter>();
+                    fitter.masterAnim = masterAnim;
+                }
             }
 
             if (__instance is OcPlSlave)
             {
                 if (modelSlave == null) modelSlave = GameObject.Instantiate<GameObject>(vrmModel);
+                var instAnim = __instance.GetComponentInChildren<Animator>();
+                var masterAnim = modelSlave.GetComponentInChildren<Animator>();
                 modelSlave.transform.SetParent(__instance.transform, false);
-                modelSlave.GetComponentInChildren<Animator>().runtimeAnimatorController = __instance.GetComponentInChildren<Animator>().runtimeAnimatorController;
+                masterAnim.runtimeAnimatorController = instAnim.runtimeAnimatorController;
+
+                var fitter = __instance.GetComponent<AnimFitter>();
+                if (fitter == null)
+                {
+                    fitter = __instance.gameObject.AddComponent<AnimFitter>();
+                    fitter.masterAnim = masterAnim;
+                }
             }
         }
 
